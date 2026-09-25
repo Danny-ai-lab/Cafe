4693b02b2d87610858bc186f811279930a4735d7
Danny-ai-lab <daniella.obozunya07@gmail.com>
2026-09-25 18:33:37 +0100

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function order() {
+    window.open("order.html");
+}
+
+function menu() {
+    window.location.href = "menu.html";
+}
+
+function learn() {
+    window.location.href = "learn.html";
+}
+
+function order() {
+    window.location.href = "order.html";
+}
+
+function locate() {
+    window.location.href = "location.html";
+}
+
+function view() {
+    window.location.href = "view.html";
+}
